25dd399599323d408cf437642dad5256dd82a532
mohsinkhan123456789 <140309399+mohsinkhan123456789@users.noreply.github.com>
2023-08-13 12:23:55 +0300

    Create Exercise4.ts

---
+++ Exercise4.ts
@@ -0,0 +1,10 @@
+// Famous Quote: Find a quote from a famous person you admire .Print the quote and the 
+//name of its author . Your outpot should look  somthing like the following including
+// the quote mark !.
+// Albert Einstein once said " A person who never made a mistake never anything new".
+
+// quaid e azam once said, " expect thr best , prepare for the worst".
+
+let famous_person:string ="Quid e Azam "
+let message_1:string = famous_person + "once said \"expect the best , prepare for the worst\""
+console.log(message_1);
